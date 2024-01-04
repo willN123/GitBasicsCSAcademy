271be3a921481befc1ccfff5876d1c4b54d989b0
willN123 <146558836+willN123@users.noreply.github.com>
2024-01-04 15:43:08 +0800

    Removing messages.
    Removed extra welcome messages.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
-Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
-Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
-Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
