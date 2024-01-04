7d343edf31d349655a64a025544370ad114fe6b3
willN123 <w.niland@live.com.au>
2024-01-04 15:45:47 +0800

    Revert "Removing messages."
    This reverts commit 271be3a921481befc1ccfff5876d1c4b54d989b0.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
+Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
+Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
+Console.WriteLine("Hi there, C# Academy! Updating from inside github.");
